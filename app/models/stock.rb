9042153ea684f58dc55df2ded96e241690a9efd0
EmanuelVogt <contatoemanuel3@gmail.com>
2021-11-28 19:48:03 -0300

    Add UseStock references

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,16 @@
 class Stock < ApplicationRecord
+
+  def self.new_lockup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: 'Tpk_d8a32b32a82843b09d52fd5fb31c753d',
+      secret_token: 'Tsk_95064763378f44b79196f4bc1a922e56',
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :user_stocks
   devise_for :users
   root 'welcome#index'
+  get 'my_portifolio', to: 'users#my_portifolio'
+  get 'search_stock', to: 'stocks#search'
 end
