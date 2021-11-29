127784772ea65259cd2e01a222da7f70238dc326
EmanuelVogt <contatoemanuel3@gmail.com>
2021-11-29 01:19:51 -0300

    Create delete stock action

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,3 +1,20 @@
 class UserStocksController < ApplicationController
+  def create
+    stock = Stock.check_db(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lockup(params[:ticker])
+      stock.save
+    end
+      @user_stock = UserStock.create(user: current_user, stock: stock)
+      flash[:notice] = "Stock #{stock.name} was added to you portifolio!"
+      redirect_to my_portifolio_path
+  end
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was delete sucessfuly!!"
+    redirect_to my_portifolio_path
+  end
   
 end
