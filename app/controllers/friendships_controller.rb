8b55e2bab8b9f31655e79181e5ca84026d89ebcf
EmanuelVogt <contatoemanuel3@gmail.com>
2021-12-08 19:10:05 -0300

    Create follow friend funcionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
+    current_user.friendships.build(friend_id: User.find(params[:friend]).try(:id))
+    if current_user.save
+      flash[:notice] = "Following user"
+    else
+      flash[:alert] = "There was something wrong"
+    end
+
+    redirect_to my_friends_path
   end
 
   def destroy
