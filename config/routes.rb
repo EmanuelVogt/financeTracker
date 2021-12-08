5648d62c3e69ad6b6655a47571b9ddfdb948769b
EmanuelVogt <contatoemanuel3@gmail.com>
2021-12-08 11:49:41 -0300

    add friends view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
+  resources :user_friends, only: [:create, :destroy]
   devise_for :users
   root 'welcome#index'
   get 'my_portifolio', to: 'users#my_portifolio'
   get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
 end
