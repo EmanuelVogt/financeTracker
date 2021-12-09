5306a2aef5355f834ccdb5213fca4abe2795279e
EmanuelVogt <contatoemanuel3@gmail.com>
2021-12-09 09:34:19 -0300

    Create show profile functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
-  
+
   devise_for :users
   root 'welcome#index'
   get 'my_portifolio', to: 'users#my_portifolio'
   get 'search_stock', to: 'stocks#search'
   get 'search_friend', to: 'users#search'
   get 'my_friends', to: 'users#my_friends'
+  resources :users, only: [:show]
 end
